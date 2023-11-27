5ba9318d1139972e5ff1148e3f732c84c41d842a
Ana Paula Reis <anapaulaa.pa@icloud.com>
2023-11-27 15:14:15 -0300

    projete1

---
+++ app.js
@@ -0,0 +1,10 @@
+function exibirTextoNatela(tag) {
+let campo = document. querySelector(tag);
+campo.innerHTML =texto;
+ }
+  exibirTextoNatela("h1, jogo do numero secreto ");
+exibirTextoNatela("p,escolha um numero de 1 a 10");
+
+function vericarChute() {
+    console.log('o botao foi clicado!')
+}
